aad5d3b70cdbfba4829886fac7205bd34422ef85
Mohamed Ganoub <86044052+MGanoub@users.noreply.github.com>
2024-05-05 09:46:49 +0200

    lets render icons

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -51,9 +51,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "a077feb07e2c908e4a96589a03d45252
 "icons/Icon-512.png": "ec845a56c316b55bddf2b1f29915f159",
 "icons/Icon-maskable-192.png": "e05c89961034c7653ed3a3e6ffd81bf8",
 "icons/Icon-maskable-512.png": "ec845a56c316b55bddf2b1f29915f159",
-"index.html": "da48c200b379a527b0e5510ae6c0382c",
-"/": "da48c200b379a527b0e5510ae6c0382c",
-"main.dart.js": "3357f372921e0846d85c75291a024774",
+"index.html": "9c85b8422e70b5c96ee92accdd8cfffb",
+"/": "9c85b8422e70b5c96ee92accdd8cfffb",
+"main.dart.js": "e84b76ff42ff05d04ca0873e80e0b70d",
 "manifest.json": "5806a7cb47ff7d3270bf87e77af8470c",
 "version.json": "08d0c7dcffe48b513f17d00e025db4fa"};
 // The application shell files that are downloaded before a service worker can
